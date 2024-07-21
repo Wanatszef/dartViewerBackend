77733386845efdfdeb951e168c92735496f99f6c
Kacper Wanat <kacperryszardwanat@gmail.com>
2024-07-21 12:37:43 +0200

    adding backend files

--- src/main/java/com/w4n4/dartViewer/config/FrontEndSettings.java
+++ src/main/java/com/w4n4/dartViewer/config/FrontEndSettings.java
@@ -3,5 +3,5 @@ package com.w4n4.dartViewer.config;
 
 public class FrontEndSettings
 {
-    public static final String FRONT_URL = "http://localhost:5173/";
+    public static final String FRONT_URL = "http://localhost:5172/";
 }
